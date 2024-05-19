e382b0e50954f3e55c98e43042cd7c72ac6b75a6
Replay95 <lia0528Ma@gmail.com>
2024-05-19 17:07:04 +0900

    ポケモン検索で数字を名前に変換

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -5,25 +5,27 @@ import { useSearchParams } from "react-router-dom";
 async function getPokemonDate(inputfield) {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputfield}`);
   const resBody = await res.json();
-  return resBody.sprites.front_default;
+  return resBody;
 }
 
 export function Page1() {
   const [searchParams] = useSearchParams();
   const inputfield = searchParams.get("inputfield");
   const [pokemonImage, setPokemonImage] = useState();
+  const [pokemonName, setPokemonName] = useState();
 
   useEffect(() => {
     async function getPokemon() {
       const pokemon = await getPokemonDate(inputfield);
-      setPokemonImage(pokemon);
+      setPokemonName(pokemon.species.name);
+      setPokemonImage(pokemon.sprites.front_default);
     }
     getPokemon(inputfield);
   }, [inputfield]);
 
   return (
     <>
-      <h1>{inputfield}</h1>
+      <h1>{pokemonName}</h1>
       <img src={pokemonImage} width={300} height={300} />
     </>
   );
